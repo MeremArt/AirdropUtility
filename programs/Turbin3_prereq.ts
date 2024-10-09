e2bcc1653da60f7455f50c2194332382191eacd7
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-10-10 00:01:19 +0100

    add:success

--- programs/Turbin3_prereq.ts
+++ programs/Turbin3_prereq.ts
@@ -1,57 +1,108 @@
-import idl from "../idl.json";
-
 export type Turbin3Prereq = {
-  idl: Idl;
-  version: string;
-  address: string;
+  address: "WBAQSygkwMox2VuWKU133NxFrpDZUBdvSBeaBEue2Jq";
   metadata: {
-    name: string;
-    version: string;
-    spec: string;
-    description: string;
+    name: "wba_prereq";
+    version: "0.1.0";
+    spec: "0.1.0";
+    description: "Created with Anchor";
   };
-  instructions: Array<{
-    name: string;
-    discriminator: number[];
-    accounts: Array<{
-      name: string;
-      writable?: boolean;
-      signer?: boolean;
-      pda?: {
-        seeds: Array<{
-          kind: string;
-          value?: number[];
-          path?: string;
-        }>;
+  instructions: [
+    {
+      name: "complete";
+      discriminator: [0, 77, 224, 147, 136, 25, 88, 76];
+      accounts: [
+        {
+          name: "signer";
+          writable: true;
+          signer: true;
+        },
+        {
+          name: "prereq";
+          writable: true;
+          pda: {
+            seeds: [
+              {
+                kind: "const";
+                value: [112, 114, 101, 114, 101, 113];
+              },
+              {
+                kind: "account";
+                path: "signer";
+              }
+            ];
+          };
+        },
+        {
+          name: "system_program";
+          address: "11111111111111111111111111111111";
+        }
+      ];
+      args: [
+        {
+          name: "github";
+          type: "bytes";
+        }
+      ];
+    },
+    {
+      name: "update";
+      discriminator: [219, 200, 88, 176, 158, 63, 253, 127];
+      accounts: [
+        {
+          name: "signer";
+          writable: true;
+          signer: true;
+        },
+        {
+          name: "prereq";
+          writable: true;
+        },
+        {
+          name: "system_program";
+          address: "11111111111111111111111111111111";
+        }
+      ];
+      args: [
+        {
+          name: "github";
+          type: "bytes";
+        }
+      ];
+    }
+  ];
+  accounts: [
+    {
+      name: "Q2Prereq2024";
+      discriminator: [210, 203, 168, 103, 251, 233, 204, 6];
+    }
+  ];
+  errors: [
+    {
+      code: 6000;
+      name: "InvalidGithubAccount";
+      msg: "Invalid Github account";
+    }
+  ];
+  types: [
+    {
+      name: "Q2Prereq2024";
+      type: {
+        kind: "struct";
+        fields: [
+          {
+            name: "github";
+            type: "bytes";
+          },
+          {
+            name: "key";
+            type: "pubkey";
+          }
+        ];
       };
-    }>;
-    args: Array<{
-      name: string;
-      type: string;
-    }>;
-  }>;
-  accounts: Array<{
-    name: string;
-    discriminator: number[];
-  }>;
-  errors?: Array<{
-    code: number;
-    name: string;
-    msg: string;
-  }>;
-  types: Array<{
-    name: string;
-    type: {
-      kind: string;
-      fields: Array<{
-        name: string;
-        type: string;
-      }>;
-    };
-  }>;
+    }
+  ];
 };
 export const IDL: Turbin3Prereq = {
-  version: "0.1.0", // Now matches the Turbin3Prereq type
   address: "WBAQSygkwMox2VuWKU133NxFrpDZUBdvSBeaBEue2Jq",
   metadata: {
     name: "wba_prereq",
@@ -59,5 +110,99 @@ export const IDL: Turbin3Prereq = {
     spec: "0.1.0",
     description: "Created with Anchor",
   },
-  idl: idl,
+  instructions: [
+    {
+      name: "complete",
+      discriminator: [0, 77, 224, 147, 136, 25, 88, 76],
+      accounts: [
+        {
+          name: "signer",
+          writable: true,
+          signer: true,
+        },
+        {
+          name: "prereq",
+          writable: true,
+          pda: {
+            seeds: [
+              {
+                kind: "const",
+                value: [112, 114, 101, 114, 101, 113],
+              },
+              {
+                kind: "account",
+                path: "signer",
+              },
+            ],
+          },
+        },
+        {
+          name: "system_program",
+          address: "11111111111111111111111111111111",
+        },
+      ],
+      args: [
+        {
+          name: "github",
+          type: "bytes",
+        },
+      ],
+    },
+    {
+      name: "update",
+      discriminator: [219, 200, 88, 176, 158, 63, 253, 127],
+      accounts: [
+        {
+          name: "signer",
+          writable: true,
+          signer: true,
+        },
+        {
+          name: "prereq",
+          writable: true,
+        },
+        {
+          name: "system_program",
+          address: "11111111111111111111111111111111",
+        },
+      ],
+      args: [
+        {
+          name: "github",
+          type: "bytes",
+        },
+      ],
+    },
+  ],
+  accounts: [
+    {
+      name: "Q2Prereq2024",
+      discriminator: [210, 203, 168, 103, 251, 233, 204, 6],
+    },
+  ],
+  errors: [
+    {
+      code: 6000,
+      name: "InvalidGithubAccount",
+      msg: "Invalid Github account",
+    },
+  ],
+  types: [
+    {
+      name: "Q2Prereq2024",
+      type: {
+        kind: "struct",
+        fields: [
+          {
+            name: "github",
+            type: "bytes",
+          },
+          {
+            name: "key",
+            type: "pubkey",
+          },
+        ],
+      },
+    },
+  ],
 };
